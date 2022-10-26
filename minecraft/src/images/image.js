ec7da15cd5757cba33dcbefee7e7cab671f38b19
oceansalle <oceanesalleiae@gmail.com>
2022-10-26 16:31:23 +0200

    initilization

---
+++ minecraft/src/images/image.js
@@ -0,0 +1,15 @@
+import dirtImg from './dirt.jpeg';
+import grassImg from './grass.jpeg';
+import glassImg from './glass.png';
+import logImg from './log.jpeg';
+import woodImg from './wood.png';
+
+const images = {
+	dirtImg,
+	grassImg,
+	glassImg,
+	woodImg,
+	logImg,
+}
+
+export default images
