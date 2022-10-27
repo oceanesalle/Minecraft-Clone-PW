d6193ae0dcebd14386d9c48888dd250dfa7ae859
oceansalle <oceanesalleiae@gmail.com>
2022-10-27 08:29:47 +0200

    menu

--- minecraft/src/App.js
+++ minecraft/src/App.js
@@ -4,6 +4,10 @@ import { Physics } from '@react-three/cannon';
 import { Ground } from './components/Ground';
 import { Player } from './components/Player';
 import { FPV } from './components/FPV';
+import { Cubes } from './components/Cubes';
+import { TextureSelector } from './components/TextureSelector';
+import { Menu } from './components/Menu';
+
 
 
 function App() {
@@ -14,11 +18,14 @@ function App() {
         <ambientLight intensity={0.5}/>
         <FPV/> 
         <Physics>
-          <Ground/>
           <Player/>
+          <Cubes />
+          <Ground/>
         </Physics>
       </Canvas>
       <div className='absolute centered cursor'></div>
+      <TextureSelector />
+      <Menu />
     </>
   );
 }
